26eb55899c21d736ead0ea741083affd2a864be8
GeorgCantor <cupsman1986@gmail.com>
2020-10-23 19:11:26 +0300

    Initial commit

---
+++ app/src/main/java/com/georgcantor/hilt/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.georgcantor.hilt.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.georgcantor.hilt.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
